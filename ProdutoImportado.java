3a93e2f879c290d2ede66dabc35ef92af703d5df
karen <karenamanda56@gmail.com>
2022-12-10 13:34:01 -0300

    Herança e Polimorfismo

---
+++ ProdutoImportado.java
@@ -0,0 +1,29 @@
+public class ProdutoImportado extends Produto{
+    private Double taxa;
+
+	public ProdutoImportado() {
+		super();
+	}
+
+	public ProdutoImportado(String name, Double price, Double taxa) {
+		super(name, price);
+		this.taxa = taxa;
+	}
+
+	public Double getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(Double taxa) {
+		this.taxa = taxa;
+	}
+
+	public double total() {
+		return super.getPreco() + taxa;
+	}
+
+	@Override
+	public String etiquetaPreco() {
+		return super.getNome() + String.format(" $ %.2f", total()) + String.format(" (Taxa: $ %.2f", taxa) + ")";
+	}
+}
---
+++ ProdutoUsado.java
@@ -0,0 +1,31 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ProdutoUsado extends Produto{
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	private Date dataManufatura;
+
+	public ProdutoUsado() {
+		super();
+	}
+
+	public ProdutoUsado(String name, Double price, Date dataManufatura) {
+		super(name, price);
+		this.dataManufatura = dataManufatura;
+	}
+
+	public Date getDataManufatura() {
+		return dataManufatura;
+	}
+
+	public void setDataManufatura(Date dataManufatura) {
+		this.dataManufatura = dataManufatura;
+	}
+
+	@Override
+	public String etiquetaPreco() {
+		return super.getNome() + " (usado) " + String.format("$ %.2f", super.getPreco()) + " Data Manufatura: ("
+				+ sdf.format(dataManufatura) + ")";
+	}
+}
